c845d412b8243b2c627a103eb2509be8ff403c02
Avngarde <pivotguy888@gmail.com>
2025-06-13 21:34:18 +0200

    Add CalculateCurrentAndEventFinishDateDifference

--- DatezTests/EventsTest.cs
+++ DatezTests/EventsTest.cs
@@ -8,7 +8,7 @@ using System.Data.Common;
 namespace DatezTests;
 
 public class EventsTests
-{ 
+{
     SQLiteAsyncConnection connection = new SQLiteAsyncConnection(":memory:", true);
 
     [Fact]
@@ -22,5 +22,36 @@ public class EventsTests
 
         Assert.Equal(1, addResult);
         Assert.Equal(1, deleteResult);
+    }
+
+    [Fact]
+    public async Task AddTest_EditTest_GetTest_ReturnsEditedName()
+    {
+        await connection.CreateTableAsync<Event>();
+        EventDatabase eventDatabase = new(connection);
+        Event ev = new Event() { Id = 1, Name = "Test", CreateDate = DateTime.Now, Description = "Test" };
+        var addResult = await eventDatabase.Add(ev);
+
+        ev.Name = "TestEdited";
+        var editResult = await eventDatabase.Edit(ev);
+
+        var updatedEv = await eventDatabase.Get(1);
+
+        Assert.Equal(1, addResult);
+        Assert.Equal(1, editResult);
+        Assert.Equal("TestEdited", updatedEv.Name);
+    }
+
+    [Fact]
+    public async Task CalculateCurrentAndEventFinishDateDifference()
+    {
+        DateTime finish = DateTime.Parse("2025-01-04 12:23");
+        DateTime currentDate = DateTime.Parse("2024-01-02 10:30");
+        Event ev = new Event() { Id = 1, Name = "Test", EventDate = finish };
+
+        TimeSpan diff = ev.EventDate - currentDate;
+        Assert.Equal(368, diff.Days);
+        Assert.Equal(1, diff.Hours);
+        Assert.Equal(53, diff.Minutes);
     }
 }
