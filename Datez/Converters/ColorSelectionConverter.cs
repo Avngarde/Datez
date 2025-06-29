751b854107979fd9ee393611712e0148a6ba0710
Avngarde <pivotguy888@gmail.com>
2025-06-29 12:20:18 +0200

    Add selected event color border width converter

---
+++ Datez/Converters/ColorSelectionConverter.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Globalization;
+
+namespace Datez.Converters;
+
+public class ColorSelectionConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        var selectedColor = value as string;
+        var buttonColor = parameter as string;
+
+        return selectedColor == buttonColor ? 4 : 0;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
