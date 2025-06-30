2c3ca3c33542c58e54aa17bb4aaeee46021c0ff1
Avngarde <pivotguy888@gmail.com>
2025-06-30 14:35:33 +0200

    Add DeleteEventCommand

--- Datez/ViewModels/EventPageViewModel.cs
+++ Datez/ViewModels/EventPageViewModel.cs
@@ -1,7 +1,10 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
-
+using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.Messaging;
+using Datez.Db;
 using Datez.Helpers.Models;
-
+using Datez.Messages;
+using Datez.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,9 +16,31 @@ namespace Datez.ViewModels
     public partial class EventPageViewModel : ObservableObject
     {
         public EventUIModel? Event { get; set; }
+        private readonly IServiceProvider _serviceProvider;
+        private readonly IDatabase<Event> _eventDb;
+
         [ObservableProperty] private string _eventName;
         [ObservableProperty] private string? _timeLeft;
         [ObservableProperty] private string? _eventColor;
+
+
+        [RelayCommand]
+        public async Task DeleteEvent()
+        {
+            var ev = await _eventDb.Get(Event.Id);
+            int result = await _eventDb.Delete(ev);
+            if (result > 0)
+            {
+                WeakReferenceMessenger.Default.Send(new RefreshEventsMessage());
+                await Application.Current.MainPage.Navigation.PopAsync(true);
+            }
+        }
+
+        public EventPageViewModel(IDatabase<Event> eventDatabase, IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+            _eventDb = eventDatabase;
+        }
 
         public void LoadEvent()
         {
