4d1758bdacc46cd083615853f9cdeac560403a9a
Avngarde <pivotguy888@gmail.com>
2025-07-04 18:34:27 +0200

    Change Name and Description to Content in Note

--- Datez/Models/Note.cs
+++ Datez/Models/Note.cs
@@ -7,7 +7,6 @@ public class Note
 {
     [PrimaryKey, AutoIncrement]
     public int Id { get; set; }
-    public string? Name { get; set; }
-    public string? Description { get; set; }
+    public string? Content { get; set; }
     public int EventId { get; set; }
 }
